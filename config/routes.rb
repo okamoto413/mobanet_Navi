26115e7c1471e4ba75e3eb51601eff3500e8a3c3
kujira12 <ha9botan@gmail.com>
2024-08-14 17:48:22 +0900

    rails_admin.rb　追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  #ユーザー認証機能
   devise_for :users
-  resources :users
-  resources :plans
-  resources :user_inputs
-  resources :recommended_plans
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root 'pages#home'
+  
+  resources :users, only: [:show, :edit, :update]
+  resources :plans, only: [:index, :show]
+  resources :user_inputs, only: [:new, :create, :edit, :update]
+  resources :recommended_plans, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
